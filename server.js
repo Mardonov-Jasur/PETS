9fe96e8a2e71cf2e07a8bc11a41a5ffeb8e775cd
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2024-01-19 20:11:25 +0900

    start project

---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+
+const connectionString = process.env.MONGO_URL;
+
+mongoose.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, goose) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection success");
+      // console.log(goose);
+      const server = require("./app");
+      let PORT = process.env.PORT || 5001;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running succesfully on PORT ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
